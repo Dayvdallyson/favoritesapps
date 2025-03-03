dcc39518dcd5db7d7d20d407c6aea0283f72f451
dayvdallyson <dayvdallyson21@gmail.com>
2025-03-02 21:43:53 -0300

    dayvdallyson commit

--- src/app/(public)/components/LoginForm.tsx
+++ src/app/(public)/components/LoginForm.tsx
@@ -69,7 +69,7 @@ export function LoginForm({
             <div className="mt-4 text-center text-sm">
               Don&apos;t have an account?{" "}
               <a href="#" className="underline underline-offset-4">
-                Sign up
+                Sign Up
               </a>
             </div>
           </form>
